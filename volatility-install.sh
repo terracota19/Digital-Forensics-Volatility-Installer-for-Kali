921f56af3b2b752876f45eff8b9cc66838f8aed4
terracota19 <198642304+terracota19@users.noreply.github.com>
2025-04-28 10:25:16 +0000

    Update volatility-install.sh

--- volatility-install.sh
+++ volatility-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd ~/
 mkdir -p scripts && cd scripts
-sudo apt-get install virtualenv python-dev git build-essential libssl-dev libffi-dev python2.7-dev -y
 
 if [ ! -d "venv" ]; then
     virtualenv -p python2 venv
@@ -9,11 +8,11 @@ fi
 
 source venv/bin/activate
 wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+
 python2 get-pip.py
 pip2 install --upgrade setuptools
 pip2 install pycryptodome distorm3==3.3.4  # versión compatible con Python 2
-deactivate
 
 if [ ! -d "volatility" ]; then
-    virtualenv -p python2 volatility
+    python2 setup.py install
 fi
