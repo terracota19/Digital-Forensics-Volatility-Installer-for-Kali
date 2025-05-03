9bd9ce58533ae50c4d325531945629672a0b76b4
terracota19 <198642304+terracota19@users.noreply.github.com>
2025-05-03 08:30:18 +0000

    Update volatility-install.sh

--- volatility-install.sh
+++ volatility-install.sh
@@ -4,18 +4,20 @@ cd ~/
 mkdir -p scripts && cd scripts
 
 if [ ! -d "venv" ]; then
+    sudo apt update
+    sudo apt install -y build-essential git libdistorm3-dev yara libraw1394-11 libcapstone-dev capstone-tool tzdata python2 python2.7-dev libpython2-dev virtualenv
     virtualenv -p python2 venv
 fi
 
 source venv/bin/activate
 
 if ! command -v pip2 &> /dev/null; then
-    wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+    curl -O https://bootstrap.pypa.io/pip/2.7/get-pip.py
     python2 get-pip.py
 fi
 
-pip2 install --upgrade setuptools
-pip2 install pycryptodome distorm3==3.3.4 yara-python==3.8.1
+pip2 install --upgrade setuptools wheel
+pip2 install pycryptodome distorm3==3.3.4 yara-python==3.8.1 pillow openpyxl ujson pytz ipython capstone
 
 if [ ! -d "volatility" ]; then
     git clone https://github.com/volatilityfoundation/volatility.git
@@ -25,3 +27,5 @@ cd volatility
 python2 setup.py install
 
 deactivate
+
+echo "✅ Volatility 2.6.1 instalado correctamente en entorno virtual Python 2"
